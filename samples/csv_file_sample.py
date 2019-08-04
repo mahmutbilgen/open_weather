3358cca2912ac53fbc21333af3a7942300eaa177
Mahmut Bilgen <Mahmut.Bilgen@gmail.com>
2019-08-04 12:03:29 -0700

    CSV file read and listing

---
+++ samples/csv_file_sample.py
@@ -0,0 +1,18 @@
+'''
+Printing CSV file
+Sample File : Google Stock Market Data - google_stock_data.csv
+'''
+
+path="./google_stock_data.csv"
+
+lines = [line for line in open(path)]
+
+# print(lines[0])
+
+# print(lines[1])
+
+dataset  = [line.strip().split(',') for line in open(path)]
+print(dataset[0])
+print(dataset[1][0])
+
+
