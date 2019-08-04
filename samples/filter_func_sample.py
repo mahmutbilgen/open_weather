ca795591004bfc45b521aaa0f9e216069337598d
Mahmut Bilgen <Mahmut.Bilgen@gmail.com>
2019-08-04 15:06:44 -0700

    Filter Function updated

--- samples/filter_func_sample.py
+++ samples/filter_func_sample.py
@@ -1,7 +1,5 @@
-
 '''
-Filter Function
-
+Filter Function : Appling filter on List Data
 '''
 import statistics
 
@@ -12,7 +10,21 @@ avg=statistics.mean(data)
 
 #filter(lambda x: x>avg, data)
 
-print (f'Original data :{data} is like these')
+print (f'Original data :{data} ')
+print(f" Greater than AVG :")
 print(list(filter(lambda x: x>avg, data)))
+print(f" Lower than AVG :")
+print(list(filter(lambda x: x<avg, data)))
 
-prilist(filter(lambda x: x<avg, data)))
+'''
+Output:
+
+Original data :[1.3, 2.7, 0.8, 4.1, 4.3, -0.1] 
+ 
+ Greater than AVG :
+[2.7, 4.1, 4.3]
+
+ Lower than AVG :
+[1.3, 0.8, -0.1]
+
+'''
